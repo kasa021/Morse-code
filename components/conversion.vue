9bfc054304bf8e4d153da6375f9332e0ade8f56e
kasa021 <mkafa.k11@gmail.com>
2023-03-13 20:32:52 +0900

    平仮名とアルファベットをモールス信号に変換するように設定

--- components/conversion.vue
+++ components/conversion.vue
@@ -2,13 +2,12 @@
   <div class="input-body">
     <div class="input-text">
       <p class="sentense">平仮名かアルファベットを入力して</p>
-      <textarea type="text" placeholder="文字を入力してください" class="input-box">
-        </textarea>
+      <textarea type="text" placeholder="文字を入力してください" class="input-box" id="textarea1"></textarea>
+      <button @click="convert()">変換</button>
     </div>
     <div class="out-text">
       <p class="sentense">変換された信号がでるよ</p>
-      <textarea type="text" placeholder="モールス信号に変換されます" class="out-box">
-        </textarea>
+      <textarea type="text" placeholder="モールス信号に変換されます" class="out-box" id="textarea2"></textarea>
     </div>
   </div>
 </template>
@@ -41,58 +40,136 @@ morseCodeMap.set('W', '.--');
 morseCodeMap.set('X', '-..-');
 morseCodeMap.set('Y', '-.--');
 morseCodeMap.set('Z', '--..');
-morseCodeMap.set('あ', '.-');
-morseCodeMap.set('い', '..');
+morseCodeMap.set('a', '.-');
+morseCodeMap.set('b', '-...');
+morseCodeMap.set('c', '-.-.');
+morseCodeMap.set('d', '-..');
+morseCodeMap.set('e', '.');
+morseCodeMap.set('f', '..-.');
+morseCodeMap.set('g', '--.');
+morseCodeMap.set('h', '....');
+morseCodeMap.set('i', '..');
+morseCodeMap.set('j', '.---');
+morseCodeMap.set('k', '-.-');
+morseCodeMap.set('l', '.-..');
+morseCodeMap.set('m', '--');
+morseCodeMap.set('n', '-.');
+morseCodeMap.set('o', '---');
+morseCodeMap.set('p', '.--.');
+morseCodeMap.set('q', '--.-');
+morseCodeMap.set('r', '.-.');
+morseCodeMap.set('s', '...');
+morseCodeMap.set('t', '-');
+morseCodeMap.set('u', '..-');
+morseCodeMap.set('v', '...-');
+morseCodeMap.set('w', '.--');
+morseCodeMap.set('x', '-..-');
+morseCodeMap.set('y', '-.--');
+morseCodeMap.set('z', '--..');
+morseCodeMap.set('あ', '--.--');
+morseCodeMap.set('い', '.-');
 morseCodeMap.set('う', '..-');
-morseCodeMap.set('え', '.');
-morseCodeMap.set('お', '---');
-morseCodeMap.set('か', '-.-');
-morseCodeMap.set('き', '-.-.');
-morseCodeMap.set('く', '--.-');
+morseCodeMap.set('え', '-.---');
+morseCodeMap.set('お', '.-...');
+morseCodeMap.set('か', '.-..');
+morseCodeMap.set('き', '-.-..');
+morseCodeMap.set('く', '...-');
 morseCodeMap.set('け', '-.--');
-morseCodeMap.set('こ', '--.');
-morseCodeMap.set('さ', '.-..');
-morseCodeMap.set('し', '...');
-morseCodeMap.set('す', '...-');
-morseCodeMap.set('せ', '--..');
-morseCodeMap.set('そ', '-..-');
-morseCodeMap.set('た', '-..');
-morseCodeMap.set('ち', '-.--.');
-morseCodeMap.set('つ', '-...');
-morseCodeMap.set('て', '-');
-morseCodeMap.set('と', '..-.');
-morseCodeMap.set('な', '.');
-morseCodeMap.set('に', '.--');
-morseCodeMap.set('ぬ', '-.--');
-morseCodeMap.set('ね', '-..-');
-morseCodeMap.set('の', '-.');
-morseCodeMap.set('は', '....');
-morseCodeMap.set('ひ', '.---');
-morseCodeMap.set('ふ', '..-.');
-morseCodeMap.set('へ', '-.-..');
-morseCodeMap.set('ほ', '.--.');
-morseCodeMap.set('ま', '-.-');
-morseCodeMap.set('み', '--');
-morseCodeMap.set('む', '---.');
-morseCodeMap.set('め', '--.-');
-morseCodeMap.set('も', '....-');
-morseCodeMap.set('や', '-.-');
-morseCodeMap.set('ゆ', '.--.-');
-morseCodeMap.set('よ', '-..');
-morseCodeMap.set('ら', '.-.');
-morseCodeMap.set('り', '...');
-morseCodeMap.set('る', '.-..-');
-morseCodeMap.set('れ', '-.-.');
-morseCodeMap.set('ろ', '...');
-morseCodeMap.set('わ', '.--');
-morseCodeMap.set('を', '.-.-');
-morseCodeMap.set('ん', '--.--');
-morseCodeMap.set('゛', '.-..-.');
-morseCodeMap.set('゜', '.-.-.-');
+morseCodeMap.set('こ', '----');
+morseCodeMap.set('さ', '-.-.-');
+morseCodeMap.set('し', '--.-.');
+morseCodeMap.set('す', '---.-');
+morseCodeMap.set('せ', '.---.');
+morseCodeMap.set('そ', '---.');
+morseCodeMap.set('た', '-.');
+morseCodeMap.set('ち', '..-.');
+morseCodeMap.set('つ', '.--.');
+morseCodeMap.set('て', '.-.--');
+morseCodeMap.set('と', '..-..');
+morseCodeMap.set('な', '.-.');
+morseCodeMap.set('に', '-.-.');
+morseCodeMap.set('ぬ', '....');
+morseCodeMap.set('ね', '--.-');
+morseCodeMap.set('の', '..--');
+morseCodeMap.set('は', '-...');
+morseCodeMap.set('ひ', '--..-');
+morseCodeMap.set('ふ', '--..');
+morseCodeMap.set('へ', '.');
+morseCodeMap.set('ほ', '-..');
+morseCodeMap.set('ま', '-..-');
+morseCodeMap.set('み', '..-.-');
+morseCodeMap.set('む', '-');
+morseCodeMap.set('め', '-...-');
+morseCodeMap.set('も', '-..-.');
+morseCodeMap.set('や', '.--');
+morseCodeMap.set('ゆ', '-..--');
+morseCodeMap.set('よ', '--');
+morseCodeMap.set('ら', '...');
+morseCodeMap.set('り', '--.');
+morseCodeMap.set('る', '-.--.');
+morseCodeMap.set('れ', '---');
+morseCodeMap.set('ろ', '.-.-');
+morseCodeMap.set('わ', '-.-');
+morseCodeMap.set('を', '.---');
+morseCodeMap.set('ん', '.-.-.');
+morseCodeMap.set('゛', '..');
+morseCodeMap.set('゜', '..--.');
 morseCodeMap.set('.', '.-.-.-');
 morseCodeMap.set(',', '--..--');
-morseCodeMap.set('。', '.-.-.-');
-morseCodeMap.set('、', '--..--');
+morseCodeMap.set('が', '.-.. ..');
+morseCodeMap.set('ぎ', '-.-.. ..');
+morseCodeMap.set('ぐ', '...- ..');
+morseCodeMap.set('げ', '-.-- ..');
+morseCodeMap.set('ご', '---- ..');
+morseCodeMap.set('ざ', '-.-.- ..');
+morseCodeMap.set('じ', '--.-. ..');
+morseCodeMap.set('ず', '---.- ..');
+morseCodeMap.set('ぜ', '.---. ..');
+morseCodeMap.set('ぞ', '---. ..');
+morseCodeMap.set('だ', '-. ..');
+morseCodeMap.set('ぢ', '..-. ..');
+morseCodeMap.set('づ', '.--. ..');
+morseCodeMap.set('で', '.-.-- ..');
+morseCodeMap.set('ど', '..-.. ..');
+morseCodeMap.set('ば', '-... ..');
+morseCodeMap.set('び', '--..-. ..');
+morseCodeMap.set('ぶ', '--.. ..');
+morseCodeMap.set('べ', '. ..');
+morseCodeMap.set('ぼ', '-.. ..');
+morseCodeMap.set('ぱ', '-... ..--.');
+morseCodeMap.set('ぴ', '--..-. ..--.');
+morseCodeMap.set('ぷ', '--.. ..--.');
+morseCodeMap.set('ぺ', '. ..--.');
+morseCodeMap.set('ぽ', '-.. ..--.');
+
+
+const convert = () => {
+  const inputText = document.getElementById('textarea1') as HTMLTextAreaElement;
+  // console.log(inputText.value);
+  const outText = (text: string) => {
+    let outText = '';
+    for (let i = 0; i < text.length; i++) {
+      const char = text.charAt(i);
+      if (morseCodeMap.has(char)) {
+        outText += morseCodeMap.get(char);
+        outText+= ' ';
+      } else {
+        outText += char;
+      }
+    }
+    return outText;
+  };
+
+
+  console.log(outText(inputText.value));
+  // console.log(morseCodeMap.get('あ'));
+
+  document.getElementById('textarea2').value = outText(inputText.value);
+
+};
+
+
+
 </script>
 
 <style scoped>
@@ -137,5 +214,4 @@ morseCodeMap.set('、', '--..--');
   justify-content: center;
   margin: 0;
 }
-  
 </style>
